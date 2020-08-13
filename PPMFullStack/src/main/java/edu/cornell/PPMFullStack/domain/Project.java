551b36a4eea650fa40fbeefa46f2656cf6863816
Alex000-eng <ruichen.dong@outlook.com>
2020-08-12 21:05:01 -0400

    2

--- PPMFullStack/src/main/java/edu/cornell/PPMFullStack/domain/Project.java
+++ PPMFullStack/src/main/java/edu/cornell/PPMFullStack/domain/Project.java
@@ -1,5 +1,111 @@
 package edu.cornell.PPMFullStack.domain;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    private String projectName;
+
+    private String projectIdentifier;
+
+    private String description;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Date created_At;
+
+    private Date updated_At;
+
+    public Project() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id= id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName= projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier= projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description= description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate= startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate= endDate;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At= created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At= updated_At;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        created_At= new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated_At= new Date();
+    }
 
 }
---
+++ PPMFullStack/src/main/java/edu/cornell/PPMFullStack/repositories/ProjectRepository.java
@@ -0,0 +1,15 @@
+package edu.cornell.PPMFullStack.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.cornell.PPMFullStack.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+
+    Iterable<Project> findAllById(Iterable<Long> ids);
+
+}
